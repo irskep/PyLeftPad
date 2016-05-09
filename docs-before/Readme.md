36faf1bdb26ee65d9d4abf064a6b0e30788738c8
Steve Johnson <steve@graffitilabs.com>
2016-05-08 17:07:43 -0700

    hello

---
+++ docs-before/Readme.md
@@ -0,0 +1,16 @@
+# PyLeftPad v1.0
+
+PyLeftPad is a reimplmenetation of the popular JavaScript library
+[left-pad](https://github.com/stevemao/left-pad).
+
+# Table of contents
+
+* [Introduction](#introduction)
+
+# Introduction
+
+Sometimes you need to pad a string, on the left. PyLeftPad does this.
+
+# API
+
+See [The PyLeftPad API](API.md).
---
+++ leftpad.py
@@ -0,0 +1,23 @@
+def leftpad(s, n, ch=' '):
+    """
+    Adds *n* instances of *ch* on the left side of the string *s*.
+
+    Arguments:
+    - `s`: a string
+    - `n`: number of instances of *ch* to add on the left
+    - `ch`: the character to pad with. Defaults to `' '`.
+
+    Example:
+
+    ```py
+    message = "Avoid unnecessary dependencies."
+    print(leftpad(s, 2, '!'))
+    ```
+
+    Prints:
+
+    ```
+    !!Avoid unnecessary dependencies.
+    ```
+    """
+    return ch * n + s
